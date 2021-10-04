62e39f8ba17934a07337eef26cf34a9b3b3a760e
henji <zhouhaogg789@outlook.com>
2021-10-04 14:56:57 +0800

    更新登录页数据绑定及Command

--- MyToDo/MyToDo/MyToDo/ViewModels/Dialogs/LoginViewModel.cs
+++ MyToDo/MyToDo/MyToDo/ViewModels/Dialogs/LoginViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 using Prism.Services.Dialogs;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,11 @@ namespace MyToDo.ViewModels.Dialogs
 {
     public class LoginViewModel : BindableBase, IDialogAware
     {
+        public LoginViewModel()
+        {
+            ExecuteCommand = new DelegateCommand<string>(Execute);
+        }
+
         public string Title { get; set; } = "ToDo";
 
         public event Action<IDialogResult> RequestClose;
@@ -26,5 +32,52 @@ namespace MyToDo.ViewModels.Dialogs
         public void OnDialogOpened(IDialogParameters parameters)
         {
         }
+
+        #region Login
+
+        public DelegateCommand<string> ExecuteCommand { get; private set; }
+
+
+        private string userName;
+
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; RaisePropertyChanged(); }
+        }
+
+        private string passWord;
+
+        public string PassWord
+        {
+            get { return passWord; }
+            set { passWord = value; RaisePropertyChanged(); }
+        }
+
+        private void Execute(string obj)
+        {
+            switch (obj)
+            {
+                case "Login": Login(); break;
+                case "LoginOut": LoginOut(); break;
+            }
+        }
+
+        void Login()
+        {
+            if (string.IsNullOrWhiteSpace(UserName) ||
+                string.IsNullOrWhiteSpace(PassWord))
+            {
+                return;
+            }
+
+
+        }
+
+        void LoginOut()
+        {
+
+        }
+        #endregion
     }
 }
